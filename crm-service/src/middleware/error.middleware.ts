3887a7bb8dd903c81b9828d593b593f860a2e067
Scarlet Sypher <ayushjha21157@gmail.com>
2026-06-14 23:33:30 +0530

    middleware: handle application errors

---
+++ crm-service/src/middleware/error.middleware.ts
@@ -0,0 +1,12 @@
+import type { Request, Response, NextFunction } from "express";
+import { sendError } from "../utilities/response.utility.js";
+
+export const errorMiddleware = (
+  err: Error,
+  req: Request,
+  res: Response,
+  _next: NextFunction,
+): void => {
+  console.error(`[ERROR] ${err.message}`, err.stack);
+  sendError(res, "Internal server error", 500, err.message);
+};
