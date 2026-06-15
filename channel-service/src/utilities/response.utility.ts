b31b97f44aed01ee5c556a0358fd0fdbaaaba4c2
Scarlet Sypher <ayushjha21157@gmail.com>
2026-06-15 11:12:39 +0530

    api: fix optional error field in responses

--- channel-service/src/utilities/response.utility.ts
+++ channel-service/src/utilities/response.utility.ts
@@ -30,7 +30,8 @@ export const sendError = (
   const response: ApiResponse = {
     success: false,
     message,
-    error,
+    ...(error && { error }),
   };
+
   res.status(statusCode).json(response);
 };
